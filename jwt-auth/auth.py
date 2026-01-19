48963e9b9a17b04157871f7e7b113b90011597a6
Irfan <irfan93940@gmail.com>
2026-01-19 17:07:24 +0530

    JWT Authentication implementation

---
+++ jwt-auth/auth.py
@@ -0,0 +1,152 @@
+from datetime import datetime, timedelta
+from typing import Annotated, Optional
+from fastapi import Depends, HTTPException, APIRouter, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="auth/token",
+    scheme_name="Login with username & password"
+)
+
+bearer_scheme = HTTPBearer(
+    scheme_name="Enter access token"
+)
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
+    create_user_model = Users(
+        username=create_user_request.username,
+        hashed_password=bcrypt_context.hash(create_user_request.password)
+    )
+    db.add(create_user_model)
+    db.commit()
+    db.refresh(create_user_model)
+    return create_user_model
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+
+    access_token = create_access_token(user.username, user.id, timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES)))
+    return {"access_token": access_token, "token_type": "bearer"}
+    
+
+
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user or not bcrypt_context.verify(password, user.hashed_password):
+        return None
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expire = datetime.utcnow() + expires_delta
+    encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+# def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+#    try:
+#     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#     username: str = payload.get("sub")
+#     user_id: int = payload.get("id")
+#     if username is None or user_id is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Could not validate credentials",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     return {
+#         "username": username,
+#         "id": user_id
+#     }
+#    except JWTError:
+#     raise HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+
+security = HTTPBearer(
+    scheme_name="Enter your token",
+    auto_error=True
+)
+def get_current_user(
+    oauth2_token: Optional[str] = Depends(oauth2_scheme),
+    bearer_credentials: Optional[HTTPAuthorizationCredentials] = Depends(bearer_scheme),
+):
+    token = None
+
+    if bearer_credentials:
+        token = bearer_credentials.credentials
+    elif oauth2_token:
+        token = oauth2_token
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication required",
+        )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return {
+            "username": payload.get("sub"),
+            "id": payload.get("id")
+        }
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
